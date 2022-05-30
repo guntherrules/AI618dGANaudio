70c83306dd74caa7f90c83a4ce1b7416896d1740
TKehlenbach <t.kehlenbach@enlyze.com>
2022-05-30 19:28:15 +0900

    module to get value range of melspecs for normalization

---
+++ preprocessing/get_value_range.py
@@ -0,0 +1,38 @@
+from lib import specgrams_helper as spec
+import glob
+import pandas as pd
+import numpy as np
+
+def get_value_range():
+    """Get min and max values for magnitude and phase of melspectograms"""
+    spec_shape = 256
+    specgramhelper = spec.SpecgramsHelper(audio_length=64000,
+                                          spec_shape=spec_shape,
+                                          window_length=spec_shape*2,
+                                          sample_rate=16000,
+                                          mel_downscale=1,
+                                          ifreq=True,
+                                          )
+    path = '../data/train/'
+    files = glob.glob(path + '*.feather')
+    print(len(files))
+
+    magmax, magmin, pmax, pmin = -100, 100, -100, 100
+
+    for file in files:
+        sample = pd.read_feather(file)
+        audio = sample['audio'][0]
+
+        melspec = specgramhelper.wave_to_melspecgram(audio)
+        if np.max(melspec[:,:,0])>magmax:
+            magmax = np.max(melspec[:,:,0])
+        if np.max(melspec[:,:,1])>pmax:
+            pmax = np.max(melspec[:,:,1])
+        if np.min(melspec[:,:,0])<magmin:
+            magmin = np.min(melspec[:,:,0])
+        if np.min(melspec[:,:,1])<pmin:
+            pmin = np.min(melspec[:,:,1])
+
+    return (magmax, magmin, pmax, pmin)
+
+print(get_value_range())
