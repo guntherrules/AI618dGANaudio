1590839f2d92f60d3da02578dd8a8fdd96767939
TKehlenbach <t.kehlenbach@enlyze.com>
2022-06-07 18:57:21 +0900

    module to transform a subset of data to finished melspectograms

--- preprocessing/make_data_subset.py
+++ preprocessing/make_data_subset.py
@@ -11,14 +11,12 @@ select = {'instrument_family_str': None,
           'qualities': None}
 load_path = '../data/train'
 save_path = '../data/train_subset'
-print(get_selection(select, load_path))
+size = len(get_selection(select, load_path))
 
 torchDataset = dataset.RawDataset(select, load_path, resolution=8)
-for idx in range(1):#range(torchDataset.__len__()):
+for idx in range(torchDataset.__len__()):
     sample = torchDataset.__getitem__(idx)
     filename = sample[1].replace(load_path, save_path)
     specgram = sample[0].numpy()
     np.save(filename, specgram)
-    #print(type(specgram), np.shape(specgram))
-
-
+    print(idx/size)
