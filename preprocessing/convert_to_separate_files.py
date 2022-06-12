0cb184aefedfa81149c0c30e02b60abcee429e77
TKehlenbach <t.kehlenbach@enlyze.com>
2022-06-12 17:14:55 +0900

    convert whole raw dataset to separate files at once

--- preprocessing/convert_to_separate_files.py
+++ preprocessing/convert_to_separate_files.py
@@ -4,9 +4,11 @@ from lib import tfrecord_to_separate_files_converter
 import os
 
 source = os.getcwd()
-path = '../data/nsynth-train.tfrecord'
-path = os.path.join(source, path)
-save_path = os.path.join(source, '../data/melspecs/train')
+save_path = os.path.join(source, '../data/all_samples')
+partitions = ['valid', 'test', 'train']
 
-converter = tfrecord_to_separate_files_converter.TfrecordConverter(raw_dataset_path=path, save_path=save_path)
-converter.convert_dataset()
+for partition in partitions:
+    path = '../data/nsynth-{}.tfrecord'.format(partition)  # path from current dir to tfrecord-files
+    load_path = os.path.join(source, path)
+    converter = tfrecord_to_separate_files_converter.TfrecordConverter(raw_dataset_path=load_path, save_path=save_path)
+    converter.convert_dataset()
