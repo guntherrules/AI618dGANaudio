fb5d5586cc062c716f8c6245281527359b173a38
TKehlenbach <t.kehlenbach@enlyze.com>
2022-06-13 16:32:55 +0900

    comments added

--- preprocessing/make_data_subset.py
+++ preprocessing/make_data_subset.py
@@ -1,3 +1,5 @@
+'''Script to select a subset of nsynth data out of the separated dataset'''
+
 import numpy as np
 
 from lib.dataset import get_selection
@@ -9,11 +11,13 @@ select = {'instrument_family_str': None,
           'pitch_range': '0[2-8]4',
           'velocity': None,
           'qualities': None}
-load_path = '../data/train'
-save_path = '../data/train_subset'
+
+load_path = '../data/all_samples'
+save_path = '../data/all_samples_512'
 size = len(get_selection(select, load_path, 'feather'))
 
-torchDataset = dataset.RawDataset(select, load_path, resolution=8)
+torchDataset = dataset.RawDataset(select, load_path, resolution=512)
+
 for idx in range(torchDataset.__len__()):
     sample = torchDataset.__getitem__(idx)
     filename = sample[1].replace(load_path, save_path)
